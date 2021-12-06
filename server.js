040126bb428935970b70351dc897cf953ac02623
Artenks <68568918+arthemioKalil@users.noreply.github.com>
2021-12-06 16:54:55 -0300

    -atualizando cors

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 
+
+app.use(cors());
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
