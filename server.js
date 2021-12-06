849ace4b5fbdf9b5ff69f4ac6ce78ddce0b8ee47
Artenks <68568918+arthemioKalil@users.noreply.github.com>
2021-12-06 17:00:47 -0300

    -atualizando cors

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.use(bodyParser.json());
 
 
 
-app.use(cors());
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+ app.use(cors(corsOptions)) // Use this after the variable declaration
 
 //rota
 app.get("/", async (req, res) => {
