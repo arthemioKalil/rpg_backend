4a2770147b32a00d133f821611fcb3b2afdddb09
Artenks <68568918+arthemioKalil@users.noreply.github.com>
2021-12-08 10:40:19 -0300

    -atualizando express

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const Description = require('./models/Description');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-
+app.use(express.json());
 
 //  app.use(cors())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Acess-Control-Allow-Methods", 'GET,PUT,POST');
-
     app.use(cors());
+
     next();
 });
 
